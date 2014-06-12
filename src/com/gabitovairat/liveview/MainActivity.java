f37f38333c1ea8001645b19a0698d7f0eed456d3
Airat Gabitov <gabitovairat@rambler.ru>
2014-06-12 13:22:53 +0400

    - show one of rectange

--- src/com/gabitovairat/liveview/MainActivity.java
+++ src/com/gabitovairat/liveview/MainActivity.java
@@ -4,12 +4,14 @@ import com.gabitovairat.components.ZoomView;
 
 import android.app.Activity;
 import android.app.ActionBar.LayoutParams;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.util.TypedValue;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.MotionEvent;
 import android.view.ScaleGestureDetector;
+import android.widget.FrameLayout;
 import android.widget.GridView;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
@@ -36,7 +38,70 @@ public class MainActivity extends Activity
     
     mainView = (LinearLayout) findViewById(R.id.mainView);
     hostView = (RelativeLayout) findViewById(R.id.hostView);
+    
+    createWeekElements();
     // setContentView(new ZoomView(this));
+  }
+  
+  @Override
+  protected void onResume()
+  {
+    super.onResume();
+  }
+  
+  int sizeInDim(int original) 
+  {
+    float pixels = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, (original), getResources().getDisplayMetrics());
+    return (int)pixels;
+  }
+  
+  LinearLayout createLinearLayout(float weighSum, int ResourceForDraw)
+  {
+    LinearLayout LL = new LinearLayout(this);
+    //LL.setBackgroundColor(Color.CYAN);
+    LL.setBackgroundResource(ResourceForDraw);
+    LL.setOrientation(LinearLayout.HORIZONTAL);
+
+    LayoutParams LLParams = new LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT);
+    LL.setWeightSum(weighSum);
+    LL.setLayoutParams(LLParams);
+    return LL;
+  }
+  
+  LinearLayout createLinearLayout(float width, float heigh, int ResourceForDraw)
+  {
+    LinearLayout LL = new LinearLayout(this);
+    //LL.setBackgroundColor(Color.CYAN);
+    LL.setBackgroundResource(ResourceForDraw);
+    LL.setOrientation(LinearLayout.HORIZONTAL);
+
+    //LayoutParams LLParams = new LayoutParams(sizeInDim((int) (width*scaleFactor)), sizeInDim((int) (heigh*scaleFactor)), 1);
+    LayoutParams LLParams = new LayoutParams(0, LayoutParams.MATCH_PARENT, 1);
+    LL.setLayoutParams(LLParams);
+    return LL;
+  }
+  
+  
+  void createWeekElements()
+  {
+    //day in year 365-366
+    float originalWeekWidth = 2;
+    float originalWeekHeigh = 4;
+    //row 52
+    int rowCount = 10;//ear in live
+    //colomn
+    int colomnCount = 366/7;//week in year
+    
+    for (int iYear = 0; iYear != rowCount; ++iYear)
+    {
+      LinearLayout newRow = createLinearLayout((float)colomnCount,R.drawable.week_draw);
+      mainView.addView(newRow);
+      for (int iWeek = 0; iWeek != colomnCount; ++iWeek)
+      {
+        LinearLayout weekL = createLinearLayout(originalWeekWidth, originalWeekHeigh, R.drawable.week_draw);
+        newRow.addView(weekL);
+      }
+    }
   }
   
   protected void invalidate()
@@ -46,14 +111,14 @@ public class MainActivity extends Activity
       OriginalX = mainView.getWidth();
       OriginalY = mainView.getHeight();
 
-      float pixelsY = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, (OriginalY*5), getResources().getDisplayMetrics());
-      float pixelsX = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, (OriginalX*5), getResources().getDisplayMetrics());
+      float pixelsY = sizeInDim(OriginalY*5);// TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, (OriginalY*5), getResources().getDisplayMetrics());
+      float pixelsX = sizeInDim(OriginalX*5);// TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, (OriginalX*5), getResources().getDisplayMetrics());
       android.widget.FrameLayout.LayoutParams params = (android.widget.FrameLayout.LayoutParams) hostView.getLayoutParams();
       params.height = (int) pixelsY;
       params.width  = (int) pixelsX;
       hostView.setLayoutParams(params);
     }
-
+    
     float pixelsY = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, (OriginalY*scaleFactor), getResources().getDisplayMetrics());
     float pixelsX = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, (OriginalX*scaleFactor), getResources().getDisplayMetrics());
     android.widget.RelativeLayout.LayoutParams params = (android.widget.RelativeLayout.LayoutParams) mainView.getLayoutParams();
