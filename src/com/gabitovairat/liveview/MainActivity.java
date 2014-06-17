a90890e8f32bbd9bd62f9d14a13168751a2d101f
Airat Gabitov <gabitovairat@rambler.ru>
2014-06-17 21:34:12 +0400

    - zoom to current week

--- src/com/gabitovairat/liveview/MainActivity.java
+++ src/com/gabitovairat/liveview/MainActivity.java
@@ -5,6 +5,7 @@ import com.gabitovairat.components.ZoomView;
 import android.animation.LayoutTransition;
 import android.app.Activity;
 import android.app.ActionBar.LayoutParams;
+import android.content.res.Configuration;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Color;
@@ -24,19 +25,26 @@ import android.widget.ImageView;
 import android.widget.ImageView.ScaleType;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
+import android.widget.Toast;
 
 public class MainActivity extends Activity
 {
   private static float         MIN_ZOOM    = 1.0f;
-  private static float         MAX_ZOOM    = 5f;
-  int OriginalX;
-  int OriginalY;
+  private static float         MAX_ZOOM    = 35f;
+  int                          OriginalX;
+  int                          OriginalY;
 
-  private float                scaleFactor = 1.f;
+  private static float         scaleFactor = 1.f;
   private ScaleGestureDetector detector;
   
-  LinearLayout mainView; 
-  RelativeLayout hostView;
+  int yearCount   = 90; // ear in live
+  int colomnCount = 52; // 366/7;//week in year
+
+  int currentYear = 33;
+  int currentWeek = 4;
+
+  LinearLayout                 mainView;
+  RelativeLayout               hostView;
 
   @Override
   protected void onCreate(Bundle savedInstanceState)
@@ -44,134 +52,150 @@ public class MainActivity extends Activity
     super.onCreate(savedInstanceState);
     detector = new ScaleGestureDetector(this, new ScaleListener());
     setContentView(R.layout.activity_main);
-    
+
     mainView = (LinearLayout) findViewById(R.id.mainView);
     hostView = (RelativeLayout) findViewById(R.id.hostView);
-    
-    
+
     Display display = getWindowManager().getDefaultDisplay();
-    
-    //Point dispSize = new Point();
+
+    // Point dispSize = new Point();
     DisplayMetrics outMetrics = new DisplayMetrics();
     display.getMetrics(outMetrics);
-    
+
     createWeekElements(outMetrics.widthPixels, outMetrics.heightPixels);
     // setContentView(new ZoomView(this));
   }
-  
+
   @Override
   protected void onResume()
   {
     super.onResume();
   }
-  
-  int sizeInDim(int original) 
+
+  int sizeInDim(int original)
   {
-    float pixels = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, (original), getResources().getDisplayMetrics());
-    return (int)pixels;
+    float pixels = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP,
+        (original), getResources().getDisplayMetrics());
+    return (int) pixels;
   }
-  
+
   LinearLayout createLinearLayout(float weighSum, int ResourceForDraw)
   {
     LinearLayout LL = new LinearLayout(this);
-    //LL.setBackgroundColor(Color.CYAN);
+    // LL.setBackgroundColor(Color.CYAN);
     LL.setBackgroundResource(ResourceForDraw);
     LL.setOrientation(LinearLayout.HORIZONTAL);
 
-    LayoutParams LLParams = new LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT);
+    LayoutParams LLParams = new LayoutParams(LayoutParams.MATCH_PARENT,
+        LayoutParams.MATCH_PARENT);
     LL.setWeightSum(weighSum);
     LL.setLayoutParams(LLParams);
     return LL;
   }
-  
+
   FrameLayout createLinearLayout(float width, float heigh, int ResourceForDraw)
   {
     FrameLayout ladderFL = new FrameLayout(this);
-    //ladderFL.setBackgroundColor(Color.GREEN);
+    // ladderFL.setBackgroundColor(Color.GREEN);
     ladderFL.setBackgroundResource(ResourceForDraw);
-    
-    LinearLayout.LayoutParams ladderFLParams = new LinearLayout.LayoutParams(0, LinearLayout.LayoutParams.MATCH_PARENT);
+
+    LinearLayout.LayoutParams ladderFLParams = new LinearLayout.LayoutParams(0,
+        LinearLayout.LayoutParams.MATCH_PARENT);
     ladderFLParams.weight = 1f;
     ladderFL.setLayoutParams(ladderFLParams);
     return ladderFL;
   }
-  
-  Bitmap createCacheBitmapForRow(int width, int heigh, int elementCount, int resForDrawBefore, int resForDrawAfter, int resForDraw, int currentElement)
+
+  Bitmap createCacheBitmapForRow(int width, int heigh, int elementCount,
+      int resForDrawBefore, int resForDrawAfter, int resForDraw,
+      int currentElement)
   {
-    float oneElementWidth = (float)width/(float)elementCount;
-    Bitmap oneElementWidthBitmapBefore = Bitmap.createBitmap((int) oneElementWidth, heigh, Bitmap.Config.ARGB_8888);
+    float oneElementWidth = (float) width / (float) elementCount;
+    Bitmap oneElementWidthBitmapBefore = Bitmap.createBitmap(
+        (int) oneElementWidth, heigh, Bitmap.Config.ARGB_8888);
     Canvas oneElementWidthCanvasBefore = new Canvas(oneElementWidthBitmapBefore);
-    Bitmap oneElementWidthBitmapAfter = Bitmap.createBitmap((int) oneElementWidth, heigh, Bitmap.Config.ARGB_8888);
+    Bitmap oneElementWidthBitmapAfter = Bitmap.createBitmap(
+        (int) oneElementWidth, heigh, Bitmap.Config.ARGB_8888);
     Canvas oneElementWidthCanvasAfter = new Canvas(oneElementWidthBitmapAfter);
-    Bitmap oneElementWidthBitmap = Bitmap.createBitmap((int) oneElementWidth, heigh, Bitmap.Config.ARGB_8888);
+    Bitmap oneElementWidthBitmap = Bitmap.createBitmap((int) oneElementWidth,
+        heigh, Bitmap.Config.ARGB_8888);
     Canvas oneElementWidthCanvas = new Canvas(oneElementWidthBitmap);
-    
-    Drawable strokeDrawableBefore = getResources().getDrawable(resForDrawBefore);
-    strokeDrawableBefore.setBounds(0, 0, oneElementWidthBitmapBefore.getWidth(), oneElementWidthBitmapBefore.getHeight());
+
+    Drawable strokeDrawableBefore = getResources()
+        .getDrawable(resForDrawBefore);
+    strokeDrawableBefore.setBounds(0, 0,
+        oneElementWidthBitmapBefore.getWidth(),
+        oneElementWidthBitmapBefore.getHeight());
     strokeDrawableBefore.draw(oneElementWidthCanvasBefore);
-    
+
     Drawable strokeDrawableAfter = getResources().getDrawable(resForDrawAfter);
-    strokeDrawableAfter.setBounds(0, 0, oneElementWidthCanvasAfter.getWidth(), oneElementWidthCanvasAfter.getHeight());
+    strokeDrawableAfter.setBounds(0, 0, oneElementWidthCanvasAfter.getWidth(),
+        oneElementWidthCanvasAfter.getHeight());
     strokeDrawableAfter.draw(oneElementWidthCanvasAfter);
-    
+
     Drawable strokeDrawable = getResources().getDrawable(resForDraw);
-    strokeDrawable.setBounds(0, 0, oneElementWidthCanvas.getWidth(), oneElementWidthCanvas.getHeight());
+    strokeDrawable.setBounds(0, 0, oneElementWidthCanvas.getWidth(),
+        oneElementWidthCanvas.getHeight());
     strokeDrawable.draw(oneElementWidthCanvas);
-    
+
     Bitmap copy = Bitmap.createBitmap(width, heigh, Bitmap.Config.ARGB_8888);
     Canvas canvas = new Canvas(copy);
-    
+
     for (int i = 0; i != elementCount; ++i)
     {
       if (i < currentElement)
       {
-        canvas.drawBitmap(oneElementWidthBitmapBefore, oneElementWidth*i, 0, null);
+        canvas.drawBitmap(oneElementWidthBitmapBefore, oneElementWidth * i, 0,
+            null);
       }
       else if (i > currentElement)
       {
-        canvas.drawBitmap(oneElementWidthBitmapAfter, oneElementWidth*i, 0, null);
+        canvas.drawBitmap(oneElementWidthBitmapAfter, oneElementWidth * i, 0,
+            null);
       }
       else
       {
-        canvas.drawBitmap(oneElementWidthBitmap, oneElementWidth*i, 0, null);
+        canvas.drawBitmap(oneElementWidthBitmap, oneElementWidth * i, 0, null);
       }
     }
-    
+
     return copy;
   }
-  
+
   void createWeekElements(int sceenSizeX, int sceenSizeY)
   {
-    //day in year 365-366
+    // day in year 365-366
     float originalWeekWidth = 2;
     float originalWeekHeigh = 4;
-    //row 52
-    int yearCount = 90;//ear in live
-    //colomn
-    int colomnCount = 52;//366/7;//week in year
-    
-    int currentYear = 33;
-    int currentWeek = 20;
-    
+
     mainView.setWeightSum(yearCount);
-    
-    Bitmap beforeCurrentRow = createCacheBitmapForRow(sceenSizeX, sceenSizeY/yearCount, colomnCount, R.drawable.past_week_draw, R.drawable.feature_week_draw, R.drawable.week_draw, 10000);
-    Bitmap afterCurrentRow  = createCacheBitmapForRow(sceenSizeX, sceenSizeY/yearCount, colomnCount, R.drawable.past_week_draw, R.drawable.feature_week_draw, R.drawable.week_draw, -10000);
-    Bitmap CurrentRow  = createCacheBitmapForRow(sceenSizeX, sceenSizeY/yearCount, colomnCount, R.drawable.past_week_draw, R.drawable.feature_week_draw, R.drawable.week_draw, currentWeek);
+
+    Bitmap beforeCurrentRow = createCacheBitmapForRow(sceenSizeX, sceenSizeY
+        / yearCount, colomnCount, R.drawable.past_week_draw,
+        R.drawable.feature_week_draw, R.drawable.week_draw, 10000);
+    Bitmap afterCurrentRow = createCacheBitmapForRow(sceenSizeX, sceenSizeY
+        / yearCount, colomnCount, R.drawable.past_week_draw,
+        R.drawable.feature_week_draw, R.drawable.week_draw, -10000);
+    Bitmap CurrentRow = createCacheBitmapForRow(sceenSizeX, sceenSizeY
+        / yearCount, colomnCount, R.drawable.past_week_draw,
+        R.drawable.feature_week_draw, R.drawable.week_draw, currentWeek);
 
     for (int iYear = 0; iYear != yearCount; ++iYear)
     {
       if (iYear < currentYear)
       {
         FrameLayout ladderFL = new FrameLayout(this);
-        LinearLayout.LayoutParams ladderFLParams = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, 0);
+        LinearLayout.LayoutParams ladderFLParams = new LinearLayout.LayoutParams(
+            LinearLayout.LayoutParams.MATCH_PARENT, 0);
         ladderFLParams.weight = 1f;
         ladderFL.setLayoutParams(ladderFLParams);
-        
+
         ImageView imageView = new ImageView(this);
         imageView.setAdjustViewBounds(true);
         imageView.setScaleType(ScaleType.FIT_XY);
-        FrameLayout.LayoutParams imageParams = new FrameLayout.LayoutParams(FrameLayout.LayoutParams.MATCH_PARENT, FrameLayout.LayoutParams.MATCH_PARENT);
+        FrameLayout.LayoutParams imageParams = new FrameLayout.LayoutParams(
+            FrameLayout.LayoutParams.MATCH_PARENT,
+            FrameLayout.LayoutParams.MATCH_PARENT);
         imageView.setLayoutParams(imageParams);
         imageView.setImageBitmap(beforeCurrentRow);
 
@@ -181,14 +205,17 @@ public class MainActivity extends Activity
       else if (iYear > currentYear)
       {
         FrameLayout ladderFL = new FrameLayout(this);
-        LinearLayout.LayoutParams ladderFLParams = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, 0);
+        LinearLayout.LayoutParams ladderFLParams = new LinearLayout.LayoutParams(
+            LinearLayout.LayoutParams.MATCH_PARENT, 0);
         ladderFLParams.weight = 1f;
         ladderFL.setLayoutParams(ladderFLParams);
-        
+
         ImageView imageView = new ImageView(this);
         imageView.setAdjustViewBounds(true);
         imageView.setScaleType(ScaleType.FIT_XY);
-        FrameLayout.LayoutParams imageParams = new FrameLayout.LayoutParams(FrameLayout.LayoutParams.MATCH_PARENT, FrameLayout.LayoutParams.MATCH_PARENT);
+        FrameLayout.LayoutParams imageParams = new FrameLayout.LayoutParams(
+            FrameLayout.LayoutParams.MATCH_PARENT,
+            FrameLayout.LayoutParams.MATCH_PARENT);
         imageView.setLayoutParams(imageParams);
         imageView.setImageBitmap(afterCurrentRow);
 
@@ -198,14 +225,17 @@ public class MainActivity extends Activity
       else if (iYear == currentYear)
       {
         FrameLayout ladderFL = new FrameLayout(this);
-        LinearLayout.LayoutParams ladderFLParams = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, 0);
+        LinearLayout.LayoutParams ladderFLParams = new LinearLayout.LayoutParams(
+            LinearLayout.LayoutParams.MATCH_PARENT, 0);
         ladderFLParams.weight = 1f;
         ladderFL.setLayoutParams(ladderFLParams);
-        
+
         ImageView imageView = new ImageView(this);
         imageView.setAdjustViewBounds(true);
         imageView.setScaleType(ScaleType.FIT_XY);
-        FrameLayout.LayoutParams imageParams = new FrameLayout.LayoutParams(FrameLayout.LayoutParams.MATCH_PARENT, FrameLayout.LayoutParams.MATCH_PARENT);
+        FrameLayout.LayoutParams imageParams = new FrameLayout.LayoutParams(
+            FrameLayout.LayoutParams.MATCH_PARENT,
+            FrameLayout.LayoutParams.MATCH_PARENT);
         imageView.setLayoutParams(imageParams);
         imageView.setImageBitmap(CurrentRow);
 
@@ -215,11 +245,13 @@ public class MainActivity extends Activity
       else
       {
         FrameLayout ladderFL = new FrameLayout(this);
-        LinearLayout.LayoutParams ladderFLParams = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, 0);
+        LinearLayout.LayoutParams ladderFLParams = new LinearLayout.LayoutParams(
+            LinearLayout.LayoutParams.MATCH_PARENT, 0);
         ladderFLParams.weight = 1f;
         ladderFL.setLayoutParams(ladderFLParams);
-        
-        LinearLayout newRow = createLinearLayout((float)colomnCount,R.drawable.week_draw);
+
+        LinearLayout newRow = createLinearLayout((float) colomnCount,
+            R.drawable.week_draw);
         for (int iWeek = 0; iWeek != colomnCount; ++iWeek)
         {
           int WeekDrawable = 0;
@@ -235,8 +267,9 @@ public class MainActivity extends Activity
           {
             WeekDrawable = R.drawable.week_draw;
           }
-          
-          FrameLayout weekL = createLinearLayout(originalWeekWidth, originalWeekHeigh, WeekDrawable);
+
+          FrameLayout weekL = createLinearLayout(originalWeekWidth,
+              originalWeekHeigh, WeekDrawable);
           newRow.addView(weekL);
         }
         ladderFL.addView(newRow);
@@ -244,7 +277,7 @@ public class MainActivity extends Activity
       }
     }
   }
-  
+
   protected void invalidate()
   {
     if (OriginalX == 0)
@@ -252,27 +285,48 @@ public class MainActivity extends Activity
       OriginalX = mainView.getWidth();
       OriginalY = mainView.getHeight();
 
-      float pixelsY = sizeInDim(OriginalY*5);// TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, (OriginalY*5), getResources().getDisplayMetrics());
-      float pixelsX = sizeInDim(OriginalX*5);// TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, (OriginalX*5), getResources().getDisplayMetrics());
-      android.widget.FrameLayout.LayoutParams params = (android.widget.FrameLayout.LayoutParams) hostView.getLayoutParams();
+      float pixelsY = sizeInDim((int) (OriginalY * MAX_ZOOM));// TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP,
+                                                              // (OriginalY*5),
+                                                              // getResources().getDisplayMetrics());
+      float pixelsX = sizeInDim((int) (OriginalX * MAX_ZOOM));// TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP,
+                                                              // (OriginalX*5),
+                                                              // getResources().getDisplayMetrics());
+      android.widget.FrameLayout.LayoutParams params = (android.widget.FrameLayout.LayoutParams) hostView
+          .getLayoutParams();
       params.height = (int) pixelsY;
-      params.width  = (int) pixelsX;
+      params.width = (int) pixelsX;
       hostView.setLayoutParams(params);
     }
-    
-    float pixelsY = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, (OriginalY*scaleFactor), getResources().getDisplayMetrics());
-    float pixelsX = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, (OriginalX*scaleFactor), getResources().getDisplayMetrics());
-    android.widget.RelativeLayout.LayoutParams params = (android.widget.RelativeLayout.LayoutParams) mainView.getLayoutParams();
+
+    float pixelsY = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP,
+        (OriginalY * scaleFactor), getResources().getDisplayMetrics());
+    float pixelsX = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP,
+        (OriginalX * scaleFactor), getResources().getDisplayMetrics());
+    android.widget.RelativeLayout.LayoutParams params = (android.widget.RelativeLayout.LayoutParams) mainView
+        .getLayoutParams();
     params.height = (int) pixelsY;
-    params.width  = (int) pixelsX;
-    
-    int xOffset = -(int) (OriginalX - OriginalX*scaleFactor);
-    int yOffset = -(int) (OriginalY - OriginalY*scaleFactor);
-    
-    params.setMargins(-1*xOffset, -1*yOffset, xOffset, yOffset);
+    params.width = (int) pixelsX;
+
+    int xOffset = -(int) ((OriginalX - OriginalX * scaleFactor) * getViewPartOffsetX());
+    int yOffset = -(int) ((OriginalY - OriginalY * scaleFactor) * getViewPartOffsetY());
+
+    // int xOffset = -(int) (OriginalX*(1-scaleFactor)*(getViewPartOffsetX()));
+    // int yOffset = -(int) (OriginalY*(1-scaleFactor)*(getViewPartOffsetY()));
+
+    params.setMargins(-1 * xOffset, -1 * yOffset, xOffset, yOffset);
     mainView.setLayoutParams(params);
   }
-  
+
+  float getViewPartOffsetX()
+  {
+    return ((float) currentWeek) / (float) (colomnCount-1);
+  }
+
+  float getViewPartOffsetY()
+  {
+    return ((float) currentYear) / (float) (yearCount-1);
+  }
+
   @Override
   public boolean onTouchEvent(MotionEvent event)
   {
